8cf506d3eb92866f37081acf536462ec8eefe677
wejick <wejick@3e3c2670-0a40-11df-aac0-2d4c26e34a4c>
2010-01-26 06:12:51 +0000

    Pertamax

---
+++ srsetinit.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+###Begin Info
+# Name : srsetinit
+# Short-Description : prepare the work mode of SR
+# Author / Copyright : Gian Giovani <wejick@gmail.com> wejick.wordpress.com
+# License : GPL
+### END Info
+
+. /etc/sisrestore.conf
+
+chmod 744 /etc/init.d/sisrestore
+
+if [ $wmode -eq 1 ]; then
+	if [ -x /etc/rcS.d/S38sisrestore ]; then
+		mv /etc/rcS.d/S38sisrestore /etc/rcS.d/K38sisrestore
+	fi
+	if [ -r /etc/rc0.d/K01sisrestore ]; then
+		mv /etc/rc0.d/K01sisrestore /etc/rc0.d/S01sisrestore
+	fi
+	if [ ! -x /etc/rc0.d/S01sisrestore ]; then
+		chmod 744 /etc/rc0.d/S01sisrestore
+	fi
+elif [ $wmode -eq 2 ]; then
+	if [ -x /etc/rc0.d/S01sisrestore ]; then
+		mv /etc/rc0.d/S01sisrestore /etc/rc0.d/K01sisrestore
+	fi
+	if [ -r /etc/rcS.d/K38sisrestore ]; then
+		mv /etc/rcS.d/K38sisrestore /etc/rcS.d/S38sisrestore
+	fi
+	if [ ! -x /etc/rcS.d/S38sisrestore ]; then
+		chmod 744 /etc/rcS.d/S38sisrestore
+	fi
+elif [ $wmode -eq 3 ]; then
+	if [ -x /etc/rcS.d/S38sisrestore ]; then
+		mv /etc/rcS.d/S38sisrestore /etc/rcS.d/K38sisrestore
+	fi
+	if [ -x /etc/rc0.d/S01sisrestore ]; then
+		mv /etc/rc0.d/S01sisrestore /etc/rc0.d/K01sisrestore
+	fi
+else
+	echo "$(date +%F===%H:%m:%S) Operation failed. See documentation. Init script setup failure" >> $log_file
+	echo "Operation failed. See documentation. Init script setup failure"
+fi
