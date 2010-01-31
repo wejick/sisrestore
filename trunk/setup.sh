6a6a2ba7c7b769fae9dc37e910903845a24b2465
wejick <wejick@3e3c2670-0a40-11df-aac0-2d4c26e34a4c>
2010-01-31 09:14:19 +0000

    version 0.9.3

--- trunk/setup.sh
+++ trunk/setup.sh
@@ -1,63 +1,91 @@
 #! /bin/sh
+. ./sisrestore.conf
 
 if [ ! $(whoami) = "root" ]; then
 	echo "I need run as root account"
 	exit
 fi
-if [ "$1" = "install" ]; then
-	cp ./SistemRestore.sh /usr/sbin/
-	chmod 744 /usr/sbin/SistemRestore.sh
-	ln -s /usr/sbin/SistemRestore.sh /usr/sbin/sisrestore
-	chmod 744 /usr/sbin/sisrestore
-	
-	cp ./xsr /usr/sbin
-	chmod 744 /usr/sbin/xsr
 
-	cp ./sisrestore /etc/init.d/
-	chmod 744 /etc/init.d/sisrestore
-	ln -s /etc/init.d/sisrestore /etc/rcS.d/S38sisrestore
-	chmod 444 /etc/init.d/sisrestore /etc/rcS.d/S38sisrestore
-	ln -s /etc/init.d/sisrestore /etc/rc0.d/S01sisrestore
-	chmod 444 /etc/rc0.d/S01sisrestore
+if [ "$target" = "default" ]; then
+	echo "Edit sisrestore.conf properly before install it"
+	echo ""
+	status=1
+else
+	status=0
+fi
 
-	mkdir /var/kompudini/
-	chmod -R 755 /var/kompudini
-	mkdir /var/kompudini/sr
-	chmod -R 755 /var/kompudini
-	touch /var/kompudini/sr/sisrestore.log
-	chmod 666 /var/kompudini/sr/sisrestore.log
-	touch /var/kompudini/sr/status
-	chmod 666 /var/kompudini/sr/status
-	chmod -R 777 /var/kompudini/sr/
+if [ "$1" = "install" ] && [ "$status" -eq 0 ]; then
+	if [ "$2" = "bsd" ] && [ "$bsdmod" -eq 1 ]; then
+		cp ./sisrestore /etc/rc.d/
+		chmod 744 /etc/rc.d/sisrestore
+		echo "bsd mod"
+		stat2=0
+	elif [ "$2" = "" ]; then
+		echo "sysV mod"
+		cp ./sisrestore /etc/init.d/
+		chmod 744 /etc/init.d/sisrestore
+		ln -s /etc/init.d/sisrestore /etc/rcS.d/S38sisrestore
+		chmod 444 /etc/init.d/sisrestore /etc/rcS.d/S38sisrestore
+		ln -s /etc/init.d/sisrestore /etc/rc0.d/S01sisrestore
+		chmod 444 /etc/rc0.d/S01sisrestore
+		stat2=0
+	else
+		echo "Check your configuration and documentation"
+		stat2=1
+	fi
 	
-	mkdir /usr/share/kompudini
-	chmod -R 755 /usr/share/kompudini
-	mkdir /usr/share/kompudini/sr
-	chmod -R 755 /usr/share/kompudini/sr
-	cp ./srsetinit.sh /usr/share/kompudini/sr/
-	chmod 744 /usr/share/kompudini/sr/srsetinit.sh
-	cp ./srhelp /usr/share/kompudini/sr/
-	chmod 644 /usr/share/kompudini/sr/srhelp
-	cp ./setup.sh /usr/share/kompudini/sr/
-	chmod 744 /usr/share/kompudini/sr/setup.sh
-	cp ./documentation.html /usr/share/kompudini/sr/
-	chmod 744 /usr/share/kompudini/sr/documentation.html
-	cp ./handbook.css /usr/share/kompudini/sr/
-	chmod 744 /usr/share/kompudini/sr/handbook.css
+	if [ "$stat2" -eq 0 ]; then
+		cp ./SistemRestore.sh /usr/sbin/
+		chmod 744 /usr/sbin/SistemRestore.sh
+		ln -s /usr/sbin/SistemRestore.sh /usr/sbin/sisrestore
+		chmod 744 /usr/sbin/sisrestore
+		
+		cp ./xsr /usr/sbin
+		chmod 744 /usr/sbin/xsr
+
+
+		mkdir /var/kompudini/
+		chmod -R 755 /var/kompudini
+		mkdir /var/kompudini/sr
+		chmod -R 755 /var/kompudini
+		touch /var/kompudini/sr/sisrestore.log
+		chmod 666 /var/kompudini/sr/sisrestore.log
+		touch /var/kompudini/sr/status
+		chmod 666 /var/kompudini/sr/status
+		chmod -R 777 /var/kompudini/sr/
 	
-	cp ./sisrestore.conf /etc/
-	chmod 644 /etc/sisrestore.conf
-	cp ./sr.desktop /usr/share/applications
-	chmod 777 /usr/share/applications/sr.desktop
-	ln /usr/share/applications/sr.desktop /home/*/Desktop
-	echo "Enable SistemRestore .."
-	sisrestore enable
-	echo "Install Finish"
+		mkdir /usr/share/kompudini
+		chmod -R 755 /usr/share/kompudini
+		mkdir /usr/share/kompudini/sr
+		chmod -R 755 /usr/share/kompudini/sr
+		cp ./srsetinit.sh /usr/share/kompudini/sr/
+		chmod 744 /usr/share/kompudini/sr/srsetinit.sh
+		cp ./srhelp /usr/share/kompudini/sr/
+		chmod 644 /usr/share/kompudini/sr/srhelp
+		cp ./setup.sh /usr/share/kompudini/sr/
+		chmod 744 /usr/share/kompudini/sr/setup.sh
+		cp ./documentation.html /usr/share/kompudini/sr/
+		chmod 744 /usr/share/kompudini/sr/documentation.html
+		cp ./handbook.css /usr/share/kompudini/sr/
+		chmod 744 /usr/share/kompudini/sr/handbook.css
+		cp ./sisrestore.conf /usr/share/kompudini/sr/sisrestore.conf
+		chmod 744 /usr/share/kompudini/sr/sisrestore.conf
+	
+		cp ./sisrestore.conf /etc/
+		chmod 644 /etc/sisrestore.conf
+		cp ./sr.desktop /usr/share/applications
+		chmod 777 /usr/share/applications/sr.desktop
+		ln -s /usr/share/applications/sr.desktop /home/*/Desktop
+		chmod 777 /home/*/Desktop/sr.desktop
+		echo "Enable SistemRestore .."
+		sisrestore enable
+		echo "Install Finish"
+	fi
 elif [ "$1" = "uninstall" ]; then
 	rm -rfv /usr/sbin/SistemRestore.sh
 	rm -rfv /usr/sbin/sisrestore
 	rm -rfv /usr/sbin/xsr
-	rm -rfv /etc/init.d/sisrestore
+	rm -rfv $INITPATH/sisrestore
 	rm -rfv /etc/rcS.d/S38sisrestore || rm -rf /etc/rcS.d/K38sisrestore
 	rm -rfv /etc/rc0.d/S01sisrestore || rm -rf /etc/rc0.d/K01sisrestore
 	rm -rfv /var/kompudini/sr
@@ -67,5 +95,7 @@ elif [ "$1" = "uninstall" ]; then
 	rm -rfv /home/*/Desktop/sr.desktop
 	echo "Uninstall Finish"
 else
-	echo "install | uninstall"
+	echo "option : install | uninstall"
+	echo "  if you install in bsd init style system use bsd option"
+	echo "  example : sh setup.sh install bsd"
 fi
--- trunk/srsetinit.sh
+++ trunk/srsetinit.sh
@@ -8,36 +8,57 @@
 
 . /etc/sisrestore.conf
 
-chmod 744 /etc/init.d/sisrestore
+if [ $bsdmod -eq 0 ]; then
+	chmod 744 $INITPATH/sisrestore
+	if [ $wmode -eq 1 ]; then
+		if [ -x /etc/rcS.d/S38sisrestore ]; then
+			mv /etc/rcS.d/S38sisrestore /etc/rcS.d/K38sisrestore
+		fi
+		if [ -r /etc/rc0.d/K01sisrestore ]; then
+			mv /etc/rc0.d/K01sisrestore /etc/rc0.d/S01sisrestore
+		fi
+		if [ ! -x /etc/rc0.d/S01sisrestore ]; then
+			chmod 744 /etc/rc0.d/S01sisrestore
+		fi
+	elif [ $wmode -eq 2 ]; then
+		if [ -x /etc/rc0.d/S01sisrestore ]; then
+			mv /etc/rc0.d/S01sisrestore /etc/rc0.d/K01sisrestore
+		fi
+		if [ -r /etc/rcS.d/K38sisrestore ]; then
+			mv /etc/rcS.d/K38sisrestore /etc/rcS.d/S38sisrestore
+		fi
+		if [ ! -x /etc/rcS.d/S38sisrestore ]; then
+			chmod 744 /etc/rcS.d/S38sisrestore
+		fi
+	elif [ $wmode -eq 3 ]; then
+		if [ -x /etc/rcS.d/S38sisrestore ]; then
+			mv /etc/rcS.d/S38sisrestore /etc/rcS.d/K38sisrestore
+		fi
+		if [ -x /etc/rc0.d/S01sisrestore ]; then
+			mv /etc/rc0.d/S01sisrestore /etc/rc0.d/K01sisrestore
+		fi
+	else
+		echo "$(date +%F===%H:%m:%S) Operation failed. See documentation. Init script setup failure" >> $log_file
+		echo "Operation failed. See documentation. Init script setup failure"
+	fi
+elif [ $bsdmod -q 1 ]; then
+	if [ $wmode -eq 1 ]; then
+		if [ -x /etc/rc.d/sisrestore ]; then
+			chmod 744 /etc/rc.d/sisrestore
+		fi
+	elif [ $wmode -eq 2 ]; then
+		if [ -x /etc/rc.d/sisrestore ]; then
+			chmod 744 /etc/rc.d/sisrestore
+		fi
+	elif [ $wmode -eq 3 ]; then
+		if [ -x /etc/rc.d/sisrestore ]; then
+			chmod 644 /etc/rc.d/sisrestore
+		fi
+	else
+		echo "$(date +%F===%H:%m:%S) Operation failed. See documentation. Init script setup failure" >> $log_file
+		echo "Operation failed. See documentation. Init script setup failure"
+	fi
 
-if [ $wmode -eq 1 ]; then
-	if [ -x /etc/rcS.d/S38sisrestore ]; then
-		mv /etc/rcS.d/S38sisrestore /etc/rcS.d/K38sisrestore
-	fi
-	if [ -r /etc/rc0.d/K01sisrestore ]; then
-		mv /etc/rc0.d/K01sisrestore /etc/rc0.d/S01sisrestore
-	fi
-	if [ ! -x /etc/rc0.d/S01sisrestore ]; then
-		chmod 744 /etc/rc0.d/S01sisrestore
-	fi
-elif [ $wmode -eq 2 ]; then
-	if [ -x /etc/rc0.d/S01sisrestore ]; then
-		mv /etc/rc0.d/S01sisrestore /etc/rc0.d/K01sisrestore
-	fi
-	if [ -r /etc/rcS.d/K38sisrestore ]; then
-		mv /etc/rcS.d/K38sisrestore /etc/rcS.d/S38sisrestore
-	fi
-	if [ ! -x /etc/rcS.d/S38sisrestore ]; then
-		chmod 744 /etc/rcS.d/S38sisrestore
-	fi
-elif [ $wmode -eq 3 ]; then
-	if [ -x /etc/rcS.d/S38sisrestore ]; then
-		mv /etc/rcS.d/S38sisrestore /etc/rcS.d/K38sisrestore
-	fi
-	if [ -x /etc/rc0.d/S01sisrestore ]; then
-		mv /etc/rc0.d/S01sisrestore /etc/rc0.d/K01sisrestore
-	fi
 else
-	echo "$(date +%F===%H:%m:%S) Operation failed. See documentation. Init script setup failure" >> $log_file
-	echo "Operation failed. See documentation. Init script setup failure"
+	echo >> "$(date +%F===%H:%m:%S) SR did not understand value of bsdmod" >> $log_file
 fi
